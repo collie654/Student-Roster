f04b2cd57931ae421b5aeff9da6ef8f5fd3ec718
collie654 <net2@pct.edu>
2026-05-17 09:01:35 -0400

    chore: set up db models, alembic db connection, updated env.py with my models, generated my first migration

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,16 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-from app.config import settings
+# Grabbing db connection string from env variable
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(settings.database_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# SQLAlchemy object that manages db connection
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True, # finds stale connections before using them
+    pool_size=10, # number of connections
+    max_overflow=20, # extra connections for heavy load
+    echo=False, # disable SQL query logging
+)
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# Factory to create db sessions
+SessionLocal = sessionmaker(
+    autocommit=False, # turned off so I can manually call session.commit() for control over transactions
+    autoflush=False, # turned off so I control when changes are sent to the db
+    bind=engine # connects session to engine
+)
